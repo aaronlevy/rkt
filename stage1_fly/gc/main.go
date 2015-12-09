15253c48b803e852db3f8f96e8960f2277a07315
Stefan Junker <mail@stefanjunker.de>
2015-12-09 15:05:29 +0100

    FIXMELATER: shortcircuit fly gc

--- stage1_fly/gc/main.go
+++ stage1_fly/gc/main.go
@@ -15,19 +15,9 @@
 package main
 
 import (
-	"bufio"
 	"flag"
-	"fmt"
 	"io/ioutil"
 	"log"
-	"os"
-	"regexp"
-	"runtime"
-	"sort"
-	"strings"
-	"syscall"
-
-	"github.com/coreos/rkt/Godeps/_workspace/src/github.com/appc/spec/schema/types"
 )
 
 const (
@@ -38,99 +28,12 @@ var (
 	debug bool
 )
 
-func init() {
-	flag.BoolVar(&debug, "debug", false, "Run in debug mode")
-
-	// this ensures that main runs only on main thread (thread group leader).
-	// since namespace ops (unshare, setns) are done for a single thread, we
-	// must ensure that the goroutine does not jump from OS thread to thread
-	runtime.LockOSThread()
-}
-
-// readLines reads a whole file into memory
-// and returns a slice of its lines.
-func readLines(path string) ([]string, error) {
-	file, err := os.Open(path)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	var lines []string
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-	return lines, scanner.Err()
-}
-
-// writeLines writes the lines to the given file.
-func writeLines(lines []string, path string) error {
-	file, err := os.Create(path)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	w := bufio.NewWriter(file)
-	for _, line := range lines {
-		fmt.Fprintln(w, line)
-	}
-	return w.Flush()
-}
-
 func main() {
 	flag.Parse()
 
 	if !debug {
 		log.SetOutput(ioutil.Discard)
 	}
-
-	podID, err := types.NewUUID(flag.Arg(0))
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "UUID is missing or malformed")
-		os.Exit(1)
-	}
-
-	log.Printf("Stage1: garbage-collecting %q", podID)
-
-	lines, err := readLines(mountinfoPath)
-	if err != nil {
-		log.Fatalf("readLines: %s", err)
-	}
-
-	regex := regexp.MustCompile(fmt.Sprintf(".*%s.*", podID))
-	mountMap := make(map[string]struct{})
-	var mountList []string
-	for _, line := range lines {
-		if regex.MatchString(line) {
-			splitLine := strings.Split(line, " ")
-			mount := splitLine[4]
-			if _, seen := mountMap[mount]; !seen {
-				mountMap[mount] = struct{}{}
-				mountList = append(mountList, mount)
-			}
-		}
-	}
-
-	if len(mountList) == 0 {
-		return
-	}
-
-	sort.Sort(sort.StringSlice(mountList))
-	for _, dest := range mountList {
-		log.Printf("Stage1: remounting %q", dest)
-		var flags uintptr = syscall.MS_REC | syscall.MS_PRIVATE
-		if err := syscall.Mount("", dest, "", flags, ""); err != nil {
-			log.Fatalf("Error remounting %q with flags %v: %v", dest, flags, err)
-		}
-	}
-
-	sort.Sort(sort.Reverse(sort.StringSlice(mountList)))
-	for _, dest := range mountList {
-		log.Printf("Stage1: Unmounting %q", dest)
-		if err := syscall.Unmount(dest, 0); err != nil {
-			log.Fatalf("Error unmounting %v: %v", dest, err)
-		}
-	}
+	log.Printf("Not doing anything since stage0 is cleaning up the mounts")
+	return
 }
